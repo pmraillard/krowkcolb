3c1ed55ca77ef4e862677df8ab0618c7d2651650
pmraillard <104978585+pmraillard@users.noreply.github.com>
2024-05-31 18:37:52 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 200 },
+            gravity: { y: 300 },
             debug: false
         }
     },
@@ -18,77 +18,42 @@ const config = {
 
 const game = new Phaser.Game(config);
 
-const GRID_SIZE = 128;
 let player;
-let blocks;
 let cursors;
-
-// Example NFT data
-const nftData = [
-    { imageUrl: 'assets/images/block.png' }, // Replace with actual NFT URLs
-    { imageUrl: 'assets/images/block.png' }, // Replace with actual NFT URLs
-];
-
-// Array to keep track of used NFTs
-let availableNfts = [...nftData];
+let cubes;
+let cubeCount = 0;
 
 function preload() {
     this.load.image('background', 'assets/background.png');
-    this.load.spritesheet('player', 'assets/player.png', { frameWidth: 32, frameHeight: 48 });
-
-    // Load NFT images dynamically
-    nftData.forEach((nft, index) => {
-        this.load.image(`nft${index}`, nft.imageUrl);
-    });
+    this.load.image('player', 'assets/player.png');
+    this.load.image('cube', 'assets/cube.png');
 }
 
 function create() {
     this.add.image(400, 300, 'background');
 
-   // blocks = this.physics.add.staticGroup();
+    cubes = this.physics.add.staticGroup();
 
-    player = this.physics.add.sprite(200, 450, 'player');
+    player = this.physics.add.sprite(100, 450, 'player');
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
 
-    this.anims.create({
-        key: 'left',
-        frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
-        frameRate: 10,
-        repeat: -1
-    });
-
-    this.anims.create({
-        key: 'turn',
-        frames: [{ key: 'player', frame: 4 }],
-        frameRate: 20
-    });
-
-    this.anims.create({
-        key: 'right',
-        frames: this.anims.generateFrameNumbers('player', { start: 5, end: 8 }),
-        frameRate: 10,
-        repeat: -1
-    });
+    this.physics.add.collider(player, cubes);
 
     cursors = this.input.keyboard.createCursorKeys();
 
-    this.input.keyboard.on('keydown-SPACE', placeBlock, this);
-
-    this.physics.add.collider(player, blocks);
-    this.physics.add.collider(blocks, blocks); // Allow blocks to collide with each other
+    this.input.on('pointerdown', function (pointer) {
+        placeCube(pointer.x, pointer.y);
+    }, this);
 }
 
 function update() {
     if (cursors.left.isDown) {
         player.setVelocityX(-160);
-        player.anims.play('left', true);
     } else if (cursors.right.isDown) {
         player.setVelocityX(160);
-        player.anims.play('right', true);
     } else {
         player.setVelocityX(0);
-        player.anims.play('turn');
     }
 
     if (cursors.up.isDown && player.body.touching.down) {
@@ -96,16 +61,10 @@ function update() {
     }
 }
 
-function placeBlock() {
-    if (availableNfts.length === 0) {
-        console.log('No more NFTs available to place as blocks');
-        return;
+function placeCube(x, y) {
+    if (cubeCount < 10) { // Limiter à 10 cubes pour éviter les abus
+        const cube = cubes.create(x, y, 'cube');
+        cube.body.updateFromGameObject();
+        cubeCount++;
     }
-
-    const x = Phaser.Math.Snap.To(player.x, GRID_SIZE);
-    const y = Phaser.Math.Snap.To(player.y - player.height, GRID_SIZE);
-    const nft = availableNfts.shift(); // Remove the first available NFT
-    const nftIndex = nftData.indexOf(nft);
-    const block = blocks.create(x, y, `nft${nftIndex}`);
-    block.setCollideWorldBounds(true);
 }
